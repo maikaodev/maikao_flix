c76a0354b795ee80b20c6e6fbde08a60ebdc6213
maikaodev <maikao.dev@gmail.com>
2023-01-12 17:57:05 -0300

    Passing the request data to the card component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,56 @@
+import { Card } from "@/components";
+import { useEffect, useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
 import "./style.css";
 
+type TopMoviesData = {
+  backdrop_path: string;
+  title: string;
+  vote_average: number;
+  id: number;
+};
+
+const movies_url = import.meta.env.VITE_API_URL_DEFAULT;
+const api_key = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+  const [topRated, setTopRated] = useState([{} as TopMoviesData]);
+  const [searchTopic, setSearchTopic] = useState<string>("movie");
+
+  const getTopRated = async () => {
+    //
+    const topRatedUrl = `${movies_url}${searchTopic}/top_rated?${api_key}&language=pt-BR&page=1&region=BR`;
+
+    const response = await fetch(topRatedUrl);
+    const data = await response.json();
+
+    try {
+      if (response.ok) {
+        setTopRated(data.results);
+        // console.log(data);
+      } else {
+        throw new Error("Ocorreu um erro inesperado!");
+      }
+    } catch (error: any) {
+      if (error) {
+        alert(error.message);
+      }
+    }
+  };
+
+  const handleClick = (event: any) => {
+    const value = event.target.attributes[0].value;
+    setSearchTopic(value);
+  };
+
+  useEffect(() => {
+    getTopRated();
+  }, []);
+
+  useEffect(() => {
+    getTopRated();
+  }, [searchTopic]);
+
   return (
     <>
       <div id="container">
@@ -28,19 +77,32 @@ const Home = () => {
             <div>
               <h2>Os mais populares</h2>
               <ul id="nav">
-                <li>Na TV</li>
-                <li>Nos Cinemas</li>
+                <li>
+                  <button onClick={handleClick} data-value="tv">
+                    Na TV
+                  </button>
+                </li>
+                <li>
+                  <button onClick={handleClick} data-value="movie">
+                    Nos Cinemas
+                  </button>
+                </li>
               </ul>
             </div>
             <div id="card_list">
               <ul>
-                <li>Cartaz</li>
-                <li>Cartaz</li>
-                <li>Cartaz</li>
-                <li>Cartaz</li>
-                <li>Cartaz</li>
-                <li>Cartaz</li>
-                <li>Cartaz</li>
+                {topRated &&
+                  topRated.map((movie, index) => {
+                    return (
+                      <Card
+                        key={index}
+                        url_image={movie.backdrop_path}
+                        title={movie.title}
+                        vote_average={movie.vote_average}
+                        id_movie={movie.id}
+                      />
+                    );
+                  })}
               </ul>
             </div>
           </section>
