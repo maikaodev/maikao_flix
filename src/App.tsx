992e71c5cfc1155d29ae09044053514fe598fbcf
maikaodev <maikao.dev@gmail.com>
2023-01-14 16:39:31 -0300

    Changing the default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { NavBar } from "./components";
+import { Footer, NavBar } from "./components";
 
 function App() {
   return (
-    <main>
+    <>
       <NavBar />
       <Outlet />
-    </main>
+      <Footer />
+    </>
   );
 }
 
