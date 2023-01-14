4c06b3112c889abf9ca7c67d0af0260170fcd36c
maikaodev <maikao.dev@gmail.com>
2023-01-14 16:40:07 -0300

    Adding form and changing the html

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@/components";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 
 export type TopMoviesData = {
@@ -48,6 +49,26 @@ const Home = () => {
     setSearchTopic(value);
   };
 
+  const navigate = useNavigate();
+
+  const input = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const { searching } = Object.fromEntries(formData.entries());
+
+    if (searching === "") {
+      alert("Insira um nome válido");
+    }
+    if (searching) {
+      navigate(`/${searching}`);
+    }
+    input.current!.value = "";
+  };
+
   useEffect(() => {
     getTopRated();
   }, []);
@@ -57,73 +78,60 @@ const Home = () => {
   }, [searchTopic]);
 
   return (
-    <>
-      <div id="container">
-        {/* HEADER */}
-        <header>
-          <h1>Bem-vindo(a).</h1>
-          <h2>Milhares de filmes, séries para descobrir. Explore já!</h2>
-          <form onSubmit={(event) => event.preventDefault()}>
-            <input
-              type="text"
-              name="procurando"
-              placeholder="Buscar por um Filme ou Série..."
-            />
-            <button type="submit">
-              <BiSearchAlt2 id="icon" />
-            </button>
-          </form>
-        </header>
-        {/* HEADER */}
+    <main id="container">
+      {/* HEADER */}
+      <header id="header">
+        <h2>Milhares de filmes, séries para descobrir. Explore já!</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="searching"
+            placeholder="Buscar por um Filme ou Série..."
+          />
+          <button type="submit">
+            <BiSearchAlt2 id="icon" />
+          </button>
+        </form>
+      </header>
+      {/* HEADER */}
 
-        {/* MAIN */}
-        <main>
-          <section id="overview_top_rated">
-            <div>
-              <h2>Os mais populares</h2>
-              <ul id="nav">
-                <li>
-                  <button onClick={handleClick} data-value="tv">
-                    Na TV
-                  </button>
-                </li>
-                <li>
-                  <button onClick={handleClick} data-value="movie">
-                    Nos Cinemas
-                  </button>
-                </li>
-              </ul>
-            </div>
-            <div id="card_list">
-              <ul>
-                {topRated &&
-                  topRated.map((movie, index) => {
-                    return (
-                      <Card
-                        key={index}
-                        url_image={movie.backdrop_path || movie.poster_path}
-                        title={movie.title || movie.name}
-                        vote_average={movie.vote_average}
-                        id_movie={movie.id}
-                        search_topic={searchTopic}
-                      />
-                    );
-                  })}
-              </ul>
-            </div>
-          </section>
-        </main>
-        {/* MAIN */}
-
-        <footer>
-          Desenvolvido por{" "}
-          <a href="https://github.com/maikaodev" target="_blank">
-            maikaodev
-          </a>
-          &copy; 2023
-        </footer>
-      </div>
-    </>
+      {/* CONTENT */}
+      <section id="overview_top_rated">
+        <div>
+          <h2>Os mais populares</h2>
+          <ul id="nav">
+            <li>
+              <button onClick={handleClick} data-value="tv">
+                Na TV
+              </button>
+            </li>
+            <li>
+              <button onClick={handleClick} data-value="movie">
+                Nos Cinemas
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div id="card_list">
+          <ul>
+            {topRated &&
+              topRated.map((movie, index) => {
+                return (
+                  <Card
+                    key={index}
+                    url_image={movie.backdrop_path || movie.poster_path}
+                    title={movie.title || movie.name}
+                    vote_average={movie.vote_average}
+                    id_movie={movie.id}
+                    search_topic={searchTopic}
+                  />
+                );
+              })}
+          </ul>
+        </div>
+      </section>
+      {/* MAIN */}
+    </main>
   );
 };
 
