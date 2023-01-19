b159e230f4891fffd7a3666feea847f6d93a0a32
maikaodev <maikao.dev@gmail.com>
2023-01-19 17:45:04 -0300

    Improving the css and adding a button in the header

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,12 +5,23 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 // Icons
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { BiCameraMovie } from "react-icons/bi";
+import { Sidebar } from "../Sidebar";
 
 // TS
 
 export const NavBar = () => {
   //
+  const [classMenu, setClassMenu] = useState<string>("hide");
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+    if (classMenu === "hide") return setClassMenu("show");
+    setClassMenu("hide");
+  };
 
   return (
     <header>
@@ -19,7 +30,12 @@ export const NavBar = () => {
           <BiCameraMovie id="icon" />
           MaikaoFlix
         </Link>
+        <button onClick={handleClick}>
+          {!isActive && <AiOutlineMenu />}
+          {isActive && <AiOutlineClose />}
+        </button>
       </nav>
+      <Sidebar classNav={classMenu} />
     </header>
   );
 };
