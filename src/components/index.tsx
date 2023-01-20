812049f3a653f225523aec4d8c5407e3e199ce80
maikaodev <maikao.dev@gmail.com>
2023-01-20 19:42:28 -0300

    Exporting InputText

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,7 @@ export * from "./Card";
 export * from "./Carousel";
 export * from "./Details";
 export * from "./Footer";
+export * from "./InputText";
 export * from "./Loading";
 export * from "./NavBar";
 export * from "./Sidebar";
