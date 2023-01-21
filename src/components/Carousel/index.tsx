6545bb5e2c216abc522cebec58ad6099bde3b4a3
maikaodev <maikao.dev@gmail.com>
2023-01-21 00:50:10 -0300

    Improving the html and css

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,5 @@
 import { releaseDate } from "@/utils/releaseDate";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 const api_image = import.meta.env.VITE_API_IMG;
@@ -7,21 +8,28 @@ export type CarouselProps = {
   title: string;
   release_date: string;
   background_url: string;
+  search_topic: string;
+  id_movie: number;
 };
 
 export const Carousel = ({ data = {} as CarouselProps }) => {
   return (
     <>
-      <div
-        id="carousel"
-        style={{ backgroundImage: `url(${api_image + data.background_url})` }}
-      >
-        <div id="carousel_content">
-          <h4>
-            {data.title + " (" + releaseDate(new Date(data.release_date)) + ")"}
-          </h4>
+      <Link id="link" to={`detalhes/${data.search_topic}/${data.id_movie}`}>
+        <div
+          id="carousel"
+          style={{ backgroundImage: `url(${api_image + data.background_url})` }}
+        >
+          <div id="carousel_content">
+            <h4>
+              {data.title +
+                " (" +
+                releaseDate(new Date(data.release_date)) +
+                ")"}
+            </h4>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
