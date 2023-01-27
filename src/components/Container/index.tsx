90e98a9a687d5f60a0da67eb6dc8c17ad28167ac
maikaodev <maikao.dev@gmail.com>
2023-01-27 16:50:28 -0300

    Container component created

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,16 @@
+// Components
+import { Footer } from "../Footer";
+import { NavBar } from "../NavBar";
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+export const Container = ({ children }: ContainerProps) => {
+  return (
+    <>
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
