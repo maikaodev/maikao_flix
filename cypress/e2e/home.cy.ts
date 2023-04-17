c905f7ffad4712393cfc49224b2ff8b827510ec4
maikaodev <maikao.dev@gmail.com>
2023-04-17 15:29:27 -0300

    test: home tests created

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,25 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Home page", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000");
-    cy.findByTestId("carousel-link").click();
+  });
+
+  it("should go to movies page", () => {
+    cy.findByTestId("movies_page").click();
+
+    cy.url().should("eq", "http://localhost:3000/categoria/filmes?page=1");
+  });
+
+  it("should go to series page", () => {
+    cy.findByTestId("series_page").click();
+
+    cy.url().should("eq", "http://localhost:3000/categoria/series?page=1");
+  });
+
+  it("Should search for the Batman movie", () => {
+    cy.findByTestId("research_input").should("be.enabled").type("Batman");
+
+    cy.get("#research_button").should("be.enabled").click();
+
+    cy.url().should("eq", "http://localhost:3000/procurando/Batman?page=1");
   });
 });
